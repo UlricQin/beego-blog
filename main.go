729d7b4f98a1cb03373d09691f8ad2bdeea6f85b
UlricQin <ulric.qin@gmail.com>
2014-04-10 08:59:59 +0800

    split blog table

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	_ "github.com/ulricqin/beego-blog/routers"
-	"github.com/ulricqin/beego-blog/g"
 	"github.com/astaxie/beego"
+	"github.com/ulricqin/beego-blog/g"
+	_ "github.com/ulricqin/beego-blog/routers"
 )
 
 func main() {
 	g.InitEnv()
 	beego.Run()
 }
-
--- models/models.go
+++ models/models.go
@@ -18,16 +18,21 @@ type Catalog struct {
 }
 
 type Blog struct {
-	Id        int64
-	Ident     string `orm:"unique"`
-	Title     string
-	Keywords  string `orm:"null"`
-	Content   string `orm:"type(text)"`
-	CatalogId int64  `orm:"index"`
-	Type      int8   /*0:original, 1:translate, 2:reprint*/
-	Status    int8   /*0:draft, 1:release*/
-	Views     int64
-	Created   time.Time `orm:"auto_now_add;type(datetime)"`
+	Id            int64
+	Ident         string `orm:"unique"`
+	Title         string
+	Keywords      string `orm:"null"`
+	CatalogId     int64  `orm:"index"`
+	BlogContentId int64  `orm:"unique"`
+	Type          int8   /*0:original, 1:translate, 2:reprint*/
+	Status        int8   /*0:draft, 1:release*/
+	Views         int64
+	Created       time.Time `orm:"auto_now_add;type(datetime)"`
+}
+
+type BlogContent struct {
+	Id      int64
+	Content string `orm:"type(text)"`
 }
 
 type Option struct {
@@ -44,6 +49,10 @@ func (*Blog) TableEngine() string {
 	return engine()
 }
 
+func (*BlogContent) TableEngine() string {
+	return engine()
+}
+
 func (*Option) TableEngine() string {
 	return engine()
 }
@@ -53,10 +62,10 @@ func engine() string {
 }
 
 func init() {
-	orm.RegisterModelWithPrefix("bb_", new(Catalog), new(Blog), new(Option))
+	orm.RegisterModelWithPrefix("bb_", new(Catalog), new(Blog), new(BlogContent), new(Option))
 }
 
 // func main() {
-// 	orm.RegisterDataBase("default", "mysql", "root:1234@/beego_blog?charset=utf8&loc=Asia%2FShanghai", 30, 200)
+// 	orm.RegisterDataBase("default", "mysql", "root:root@/beego_blog?charset=utf8&loc=Asia%2FShanghai", 30, 200)
 // 	orm.RunCommand()
 // }
