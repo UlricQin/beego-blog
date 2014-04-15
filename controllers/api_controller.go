65d0ef78fc90918cb489922ad20bb4660167b748
UlricQin <ulric.qin@gmail.com>
2014-04-15 10:25:49 +0800

    upload img in editor done

--- controllers/api_controller.go
+++ controllers/api_controller.go
@@ -1,10 +1,17 @@
 package controllers
 
 import (
-	"github.com/ulricqin/goutils/strtool"
-	"github.com/ulricqin/beego-blog/models/catalog"
-	"strings"
 	"fmt"
+	"github.com/ulricqin/beego-blog/g"
+	"github.com/ulricqin/beego-blog/models/catalog"
+	"github.com/ulricqin/goutils/filetool"
+	"github.com/ulricqin/goutils/strtool"
+	"strings"
+	"time"
+)
+
+const (
+	EDITOR_IMG_DIR = "static/uploads/editor"
 )
 
 type ApiController struct {
@@ -19,4 +26,43 @@ func (this *ApiController) Health() {
 func (this *ApiController) Md5() {
 	p := this.GetString("p")
 	this.Ctx.WriteString(strtool.Md5(strings.TrimSpace(p)))
+}
+
+func (this *ApiController) Upload() {
+	result := map[string]interface{}{
+		"success": false,
+	}
+
+	defer func() {
+		this.Data["json"] = &result
+		this.ServeJson()
+	}()
+
+	_, header, err := this.GetFile("image")
+	if err != nil {
+		return
+	}
+
+	ext := filetool.Ext(header.Filename)
+	imgPath := fmt.Sprintf("%s/%d%s", EDITOR_IMG_DIR, time.Now().Unix(), ext)
+
+	filetool.InsureDir(EDITOR_IMG_DIR)
+	err = this.SaveToFile("image", imgPath)
+	if err != nil {
+		return
+	}
+
+	imgUrl := "/" + imgPath
+	if g.UseQiniu {
+		if addr, er := g.UploadFile(imgPath, imgPath); er != nil {
+			return
+		} else {
+			imgUrl = addr
+			filetool.Unlink(imgPath)
+		}
+	}
+
+	result["link"] = imgUrl
+	result["success"] = true
+
 }
